4bb884b401eee1c7891202eed8398aa45ccdfc1a
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2026-05-19 16:05:23 +0530

    req.txt file added

--- db_c.py
+++ db_c.py
@@ -1,12 +1,13 @@
 import mysql.connector 
 import streamlit as st 
 
-conn=mysql.connector.connect(
-    host=st.secrets["Host_Name"],
-    user=st.secrets["user"],
-    database=st.secrets["database"],
-    password=st.secrets["password"],
-    port=st.secrets["port"]
+
+conn = mysql.connector.connect(
+    host=st.secrets["MYSQL_HOST"],
+    port=st.secrets["MYSQL_PORT"],
+    user=st.secrets["MYSQL_USER"],
+    password=st.secrets["MYSQL_PASSWORD"],
+    database=st.secrets["MYSQL_DB"]
 )
 
 cursor=conn.cursor(dictionary=True) # 
